bbbdefa1b5ea9782094c70610f1a8493d90c03f7
JamesCheng2004 <69053004+JamesCheng2004@users.noreply.github.com>
2020-08-01 14:15:56 +0200

    1

---
+++ Textausgabe/src/Main.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("James ist cool.");
+        //
+        System.out.println("Timon aber nicht.");
+        // habe Befehl für neue Zeile vergessen => 2 gleiche befehle
+    System.out.println("Photoshop war ein Joke, ich war da nicht mal hellwach");
+    //Weil mir langweilig war...
+    System.out.println("  Scheiß auf den Benz, stepp in einem nagelneuen R8, woah");
+    System.out.println("Du hast hundert Likes mehr? Ich hab' hundert Nike-Air ");
+    System.out.println("Check mal dein Style, dein Outfit ist nur ein Hundescheiß wert");
+    System.out.println("Ich habe so viele Sneaks");
+    System.out.println("Tret' einmal drauf, wir haben unnormal Beef System.out.println Flow' wie ein G");
+    System.out.println("Und die billigsten Sneaker in meinem Besitz sind dein Monatsverdienst");
+    System.out.println("Nikes im Haus, breit gebaut");
+    System.out.println("Digga, nenn mich Hercules, Kid");
+    System.out.println("Bruder, du wärst gerne wie ich Streck die Hand gen Himmel, dass du Sterne erwischt");
+    System.out.println("Einen auf dem Benz, Digga, drei auf mein Hemd");
+    System.out.println("Ich hab' Style und 'ne Gang, du bist scheiße und wack");
+    System.out.println("Dicka, meine Reime perfekt");
+    System.out.println("Bleibe der Chef, Scheine und Cash");
+    System.out.println("Kauf' mir kurz eben 'nen iPhone und Mac");
+    System.out.println("Dieser Mo'fucker macht noch Kleingeld mit Tracks");
+    System.out.println("Ja, Bruder, du bist neidisch, und jetzt?");
+    System.out.println("Mir egal, in welcher Scheiße du steckst");
+    System.out.println("   Brauche keine Warnschüsse");
+    System.out.println("    Bin ein so präziser Scharfschütze, treffe deine Zahnlücke");
+    System.out.println("Ein falsches Wort, ich komm' mit Krappi dann und bang' dich");
+    System.out.println("So fucking famous, selbst der Taxifahrer kennt mich");
+    System.out.println(" Everyday Saturday");
+    System.out.println("Everyday Saturday");
+    System.out.println(" Everyday Saturday");
+    System.out.println(" Everyday S-, everyday S-, everyday Saturday");
+    System.out.println("Schlauberger, guck, ich schreib' im Bett paar krasse Lines");
+    System.out.println(" Während dein Kopf hölzern wie 'ne Pressspanplatte bleibt");
+    System.out.println("  Bin der Beste grad am Mic, fahr den Benzer in meinen Nikes");
+    System.out.println("  Und dein scheiß Auto ist und bleibt 'ne Kettcar-Karre – nice");
+    System.out.println("  Fülle Parts mit Lines, habe den Bart gestylt");
+    System.out.println(" Steig ins Fahrzeug ein, trage gratis Shines");
+    System.out.println("Fahr ins Parkhaus rein");
+    System.out.println(" Und der Treffpunkt, an dem wir uns sehen, müsste ein Tatort sein");
+    System.out.println(" Digga, Mann, ich schlag' auf dich ein und du Freak fällst dann in Ohnmacht");
+    System.out.println("Der Boden färbt sich rot wie meine Yeezy-Red-October");
+    System.out.println(" Red ist der Chef, ich zerfetze dich jetzt");
+    System.out.println("Mit dem heftigsten Text, ich bin echter als echter");
+    System.out.println("Handy S6, bomb' mit Tracks das Geschäft");
+    System.out.println("Komm' mit Black-Cat-Cap, das ist heavy as rap");
+    System.out.println("Du willst auch ein Echter sein");
+    System.out.println("Das ist echt nicht leicht, du brauchst freshe Nikes");
+    System.out.println(" Und 'nen freshen Style, so wie Red, du weißt");
+    System.out.println(" Aber Bruder, halt das Rezept geheim");
+    System.out.println("Scheiß Penner, jetzt macht der Nicerapper Kohle");
+    System.out.println("Und ich tret in dein Gesicht rein mit der Flystepper-Sohle");
+    System.out.println("Baute damals Scheiße und aus Scheiße wurde Geld");
+    System.out.println("  Heute mehr Sneaker als Micheal Jordan selbst");
+    System.out.println("  Everyday Saturday");
+    System.out.println("  Everyday Saturday");
+    System.out.println(" Everyday Saturday");
+    System.out.println("  Everyday S-, everyday A-, everyday Aaturday");
+    System.out.println("  Everyday Saturday");
+    System.out.println("  Everyday Saturday");
+    System.out.println(" Everyday Saturday");
+    System.out.println("  Everyday S-, everyday S-, everyday Saturday");
+
+
+    }
+
+
+
+}
