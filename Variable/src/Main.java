9dbf6fdba69fdb31989acdbd1e437c3655d35171
JamesCheng2004 <69053004+JamesCheng2004@users.noreply.github.com>
2020-08-04 00:36:08 +0200

    1

--- Variable/src/Main.java
+++ Variable/src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[]args){
-        double a = 3.0 + 3.5;
-        double Ergebnis1 = a;
-        int Ergebnis2 = (int) (3 + (float)3.5);
+        int zahl1 = 3;
+        double zahl2 = 3.5;
+        double Ergebnis1 = zahl1 + zahl2;
+        double Ergebnis2 = zahl1 + (int)zahl2;
         System.out.println(Ergebnis2);
     }
 }
